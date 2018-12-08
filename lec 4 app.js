8d293f75d80f87d0023bdb51c85d202365d9b96b
Bahadur <Bahadur@coursera-something.com>
2018-12-08 16:40:52 +0530

    Lec 4 web file

--- lec 4 app.js
+++ lec 4 app.js
@@ -1,7 +1,11 @@
 (function () {
 'use strict';
-  angular.module('myFirstApp',[])
-.controller('MyFirstController',function(){
-
+angular.module('myFirstApp',[])
+.controller('MyFirstController',function($scope){
+  $scope.name="Bahadur";
+  $scope.sayHello=function(){
+    return "Hello Coursera!";
+  };
 });
+
 })();
